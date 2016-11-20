8b709651ace23a62327885f5c70a42c09a9cc8a2
eugene-lijin <594314976@qq.com>
2016-11-20 09:30:10 +0800

    config update

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+eugene_lijin:$1$gzjfqVob$pdOHbVuPHtQ2JiYISx80D0:LiJin:594314976@qq.com:admin,user
